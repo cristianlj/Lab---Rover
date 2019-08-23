a50c160bb62a6ad10b41ae4ad0b0e5b09a692a29
cristianlj <cristian.lj@hotmail.com>
2019-08-22 22:23:49 -0300

    first commit

---
+++ rover.js
@@ -0,0 +1,102 @@
+
+let rover = {
+    direction: "N",
+    x: 0,
+    y: 0,
+    travelLog: []
+};
+
+function turnLeft(rover) {
+    switch (rover.direction) {
+        case "N":
+            rover.direction = "W";
+            break;
+        case "W":
+            rover.direction = "S";
+            break;
+        case "S":
+            rover.direction = "E";
+            break;
+        case "E":
+            rover.direction = "N";
+            break;
+        default: "Error";
+            break;
+    }
+
+   
+
+    console.log("turnLeft was called!", rover);
+}
+
+function turnRight(rover) {
+    switch (rover.direction) {
+        case "N":
+            rover.direction = "E";
+            break;
+        case "E":
+            rover.direction = "S";
+            break;
+        case "S":
+            rover.direction = "W";
+            break;
+        case "W":
+            rover.direction = "N";
+            break;
+        default: "Error"
+            break;
+    }
+
+   // rover.travelLog.push(rover);
+    console.log("turnRight was called!", rover);
+}
+
+function moveForward(rover) {
+
+    switch (rover.direction) {
+        case "N":
+            rover.y -= 1;
+            break;
+        case "E":
+            rover.x += 1;
+            break;
+        case "S":
+            rover.y += 1;
+            break;
+        case "W":
+            rover.x -= 1;
+            break;
+        default: "Error"
+            break;
+    }
+
+    rover.travelLog.push({ x: rover.x, y: rover.y });
+
+    console.log("moveForward was called", rover)
+}
+
+
+
+function moving(directions) {
+
+    for (var i = 0; i < directions.length; i += 1) {
+
+        switch (directions[i]) {
+            case "f":
+                moveForward(rover);
+                break;
+            case "l":
+                turnLeft(rover);
+                break;
+            case "r":
+                turnRight(rover);
+                break;
+            default: "Error"
+                break;
+        }
+    }
+
+    console.log(rover);
+}
+
+moving('rffrfflfrff');
